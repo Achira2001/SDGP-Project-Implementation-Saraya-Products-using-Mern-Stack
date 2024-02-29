28a90ceb7300d43235d103dab28f90b1e7163e75
Achira2001 <achira.20220024@iit.ac.lk>
2024-02-29 17:34:12 +0530

    Update Files

---
+++ Saraya_Product/client/src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg ">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Sign In</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Sign Up
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a className="dropdown-item" href="#">Admin</a>
+                                <a className="dropdown-item" href="#">Rider</a>
+                                <a className="dropdown-item" href="#">Lorry</a>
+                                <a className="dropdown-item" href="#">Stores</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
