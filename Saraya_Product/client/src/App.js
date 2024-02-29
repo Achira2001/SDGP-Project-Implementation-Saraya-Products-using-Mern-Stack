dade5687375f002e7d71ec77b3b1e2c57ef0c78e
Achira2001 <achira.20220024@iit.ac.lk>
2024-02-29 19:03:13 +0530

    Update Files

--- Saraya_Product/client/src/App.js
+++ Saraya_Product/client/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Admin from './screens/Signup/Admin';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+
     </div>
   );
 }
--- Saraya_Product/client/src/components/Navbar.js
+++ Saraya_Product/client/src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Admin from '../screens/Signup/Admin';
+
 
 function Navbar() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg ">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="../screens/Signup/Admin"> Saraya Product</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -21,7 +23,7 @@ function Navbar() {
                                 Sign Up
                             </a>
                             <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <a className="dropdown-item" href="#">Admin</a>
+                                <a className="dropdown-item" href="">Admin</a>
                                 <a className="dropdown-item" href="#">Rider</a>
                                 <a className="dropdown-item" href="#">Lorry</a>
                                 <a className="dropdown-item" href="#">Stores</a>
