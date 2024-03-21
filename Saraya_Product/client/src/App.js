8cee50b4fd7814771cae15e3920496e4b3faba7b
Achira2001 <achira.20220024@iit.ac.lk>
2024-03-21 10:45:17 +0530

    Update Files

--- Saraya_Product/client/src/App.js
+++ Saraya_Product/client/src/App.js
@@ -1,33 +1,22 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './screens/Home';
-import Cart from './screens/Cart';
-import Signin from './screens/Signin'; 
-import Signup from './screens/Signup'; 
-import Aboutus from './screens/Aboutus';
-import { CartProvider } from 'react-use-cart';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import React from "react";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import indexRoutes from "./routes";
+import HomeComponent from "./components/Sections/HeroSection";
 
-function App() {
+const App = () => {
+
   return (
-    <div>
-      <Router>
-        <Navbar />
-        <CartProvider>
-          <Routes>
-            <Route path="/" element={<Home />} /> {/* Home component on root */}
-            <Route path="/cart" element={<Cart />} /> {/* Cart component */}
-            <Route path="/signin" element={<Signin />} /> {/* Signin component */}
-            <Route path="/signup" element={<Signup />} /> {/* Signup component */}
-            <Route path="/aboutus" element={<Aboutus />} /> {/* Signup component */}
-          </Routes>
-        </CartProvider>
-        <Footer />
-      </Router>
-    </div>
+    <Router>
+       <Routes>
+        {indexRoutes.map((prop, key) => {
+          return <Route path={prop.pathname} key={key} element={prop.component} />;
+          
+        <Route path="/" element={<HomeComponent />} />
+        })}
+       </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
--- Saraya_Product/client/src/newcomponents/Cart/Itemcard.js
+++ Saraya_Product/client/src/newcomponents/Cart/Itemcard.js
@@ -1,40 +1,47 @@
 import React, { useState } from 'react';
 import { useCart } from 'react-use-cart';
+import './itemcard.css'
 
-const Itemcard = (props) => {
+const ItemCard = (props) => {
   const { addItem } = useCart();
   const [selectedSize, setSelectedSize] = useState('50g');
 
   const prices = {
     'Curry Powder': {
+      
+      '25g': 32.50,
       '50g': 75,
       '100g': 150,
       '500g': 650,
       '1kg': 1200
     },
     'Chilli Powder': {
+      '25g': 45,
       '50g': 90,   
       '100g': 180, 
       '500g': 700, 
       '1kg': 1250  
     },
     'Chilli Pieces': {
+      '25g': 45,
       '50g': 90,
       '100g': 180,
       '500g': 700,
       '1kg': 1250
     },
     'Tumeric Powder': {
+      '25g': 75,
       '50g': 150,
-      '100g': 250,
-      '500g': 1200,
+      '100g': 300,
+      '500g': 950,
       '1kg': 1900
     },
     'Peppar Powder': {
+      '25g': 85,
       '50g': 170,
-      '100g': 330,
-      '500g': 1350,
-      '1kg': 2500
+      '100g': 340,
+      '500g': 1300,
+      '1kg': 2600
     }
   };
 
@@ -55,10 +62,13 @@ const Itemcard = (props) => {
   return (
     <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
       <div className="card p-0 overflow-hidden h-100 shadow">
+
+
         <img src={props.img} className="card-img-top img-fluid" alt={props.title} />
         <div className="card-body text-center">
           <h5 className="card-title">{props.title}</h5>
-          <div className="dropdown">
+          
+                    <div className="dropdown">
             <button
               className="btn btn-secondary dropdown-toggle"
               type="button"
@@ -70,6 +80,12 @@ const Itemcard = (props) => {
               {selectedSize}
             </button>
             <div className="custom-dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <button
+                className="custom-dropdown-item"
+                onClick={() => handleSelectSize('25g')}
+              >
+                25g
+              </button>              
               <button
                 className="custom-dropdown-item"
                 onClick={() => handleSelectSize('50g')}
@@ -96,6 +112,9 @@ const Itemcard = (props) => {
               </button>
             </div>
           </div>
+
+
+
           <h5 className="card-title">Rs. {prices[props.title][selectedSize]}</h5>
           <p className="card-text">{props.desc}</p>
           <button className="btn btn-success" onClick={handleAddToCart}>
@@ -107,4 +126,4 @@ const Itemcard = (props) => {
   );
 };
 
-export default Itemcard;
+export default ItemCard;
