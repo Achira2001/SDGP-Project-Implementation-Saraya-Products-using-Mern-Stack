922fe45f9609f7fdb87a60fce06e5ad2a8811f1a
Sandin Kodagoda <155352466+sandinkodagoda@users.noreply.github.com>
2024-03-25 00:11:31 +0530

    update

--- Saraya_Product/gui/src/App.js
+++ Saraya_Product/gui/src/App.js
@@ -1,20 +1,47 @@
-import React from "react";
-//import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import indexRoutes from "./routes";
-import HomeComponent from "./components/Sections/HeroSection";
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Main from "./Main";
+import Reg from "./newcomponents/RegisterEmployee/reg";
+import Log from "./newcomponents/Login/log";
+import Rider from "./newcomponents/Rider/rider";
+import ShopReg from "./newcomponents/RegisterShop/shopreg";
+import ProReg from "./newcomponents/RegisterProduct/productreg";
+import EmpRegLog from "./newcomponents/Registerlogin/log";
+import Cart from "./components/Cart/cartmain";
+import Cartlog from "./components/cartlogin/log";
+import Cartreg from "./components/cartregistr/reg";
+import RiderCart from "./newcomponents/Cart/cartmain";
 
 const App = () => {
+  const ridertoken = localStorage.getItem("ridertoken");
+  const usertoken = localStorage.getItem("usertoken");
 
   return (
     <Router>
-       <Routes>
-        {indexRoutes.map((prop, key) => {
-          return <Route path={prop.pathname} key={key} element={prop.component} />;
-          
-        <Route path="/" element={<HomeComponent />} />
-        })}
-       </Routes>
+      <Routes>
+        {usertoken && <Route path="/cart" exact element={<Cart />} />}
+        {ridertoken && <Route path="/rider" exact element={<Rider />} />}
+        <Route path="/" exact element={<Main />} />
+        <Route path="/regemp" exact element={<Reg />} />
+        <Route path="/log" exact element={<Log />} />
+        <Route path="/cartlog" exact element={<Cartlog />} />
+        <Route path="/shopreg" exact element={<ShopReg />} />
+        <Route path="/productreg" exact element={<ProReg />} />
+        <Route path="/empreglog" exact element={<EmpRegLog />} />
+        <Route path="/ridercart" exact element={<RiderCart />} />
+        <Route path="/cartreg" exact element={<Cartreg />} />
+        <Route
+          path="/cart"
+          exact
+          element={<Navigate replace to="/cartlog" />}
+        />
+        <Route path="/rider" exact element={<Navigate replace to="/log" />} />
+      </Routes>
     </Router>
   );
 };
--- Saraya_Product/server/controllers/auth.js
+++ Saraya_Product/server/controllers/auth.js
@@ -1,7 +1,9 @@
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 import Joi from "joi";
 import jwt from "../jwt/jwt.js";
 import adminModel from "../modules/admin.js";
+import EmployeeModel from "../modules/employee.js";
+import UserModel from "../modules/user.js";
 
 const authuser = async (req, res) => {
   try {
@@ -9,19 +11,43 @@ const authuser = async (req, res) => {
     if (error)
       return res.status(400).send({ message: error.details[0].message });
 
-    const user = await adminModel.findOne({ email: req.body.email });
-    if (!user)
+    let user;
+
+    const admin = await adminModel.findOne({ email: req.body.email });
+    const employee = await EmployeeModel.findOne({ email: req.body.email });
+    const normalUser = await UserModel.findOne({ email: req.body.email });
+
+    if (!admin && !employee && !normalUser)
       return res.status(401).send({ message: "Invalid Email or Password" });
 
-    // const validPassword = await bcrypt.compare(
-    //   req.body.password,
-    //   user.password
-    // );
-    if (!req.body.password === user.password)
+    let validPassword = false;
+
+    if (admin) {
+      validPassword = req.body.password === admin.password;
+      user = admin;
+    } else if (employee) {
+      validPassword = await bcrypt.compare(
+        req.body.password,
+        employee.password
+      );
+      user = employee;
+    } else {
+      validPassword = await bcrypt.compare(
+        req.body.password,
+        normalUser.password
+      );
+      user = normalUser;
+    }
+
+    if (!validPassword)
       return res.status(401).send({ message: "Invalid Email or Password" });
 
-    const token = jwt(adminModel);
-    res.status(200).send({ data: token, message: "logged in successfully" });
+    const token = jwt(user);
+    console.log(user._id);
+
+    res
+      .status(200)
+      .send({ data: token, message: "Logged in successfully", id: user._id });
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: "Internal Server Error" });
